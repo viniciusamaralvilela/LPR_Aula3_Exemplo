3c53d081c3d4d0bf8f6782650b86d446902c50ac
viniciusamaralvilela <amaralvilelavinicius@gmail.com>
2025-03-17 10:45:47 -0300

    aula4-clonandodoprof

--- cloneGit.cpp
+++ cloneGit.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "pt_BR.utf8"); //Para usar acentuação
-    cout << "Hello World";
     cout << "\032[1;32m***************************************************\033[0m\n";
     cout << "\032[1;32m*                                                 *\033[0m\n";
-    cout << "\032[1;32m*   \033[1;33m PARABÉNS 34DS!                     \033[1;32m          *\033[0m\n";
+    cout << "\032[1;32m*   \033[1;33m PARABÉNS Vinícius!                     \033[1;32m          *\033[0m\n";
     cout << "\032[1;32m*                                                 *\033[0m\n";
     cout << "\032[1;32m*   \033[1;36mVocê clonou um repositório público do Git     \033[1;32m*\033[0m\n";
-    cout << "\032[1;32m*   \033[1;36mpara o seu repositório local!                 \033[1;32m*\033[0m\n";
+    cout << "\032[1;32m*   \033[1;36mpara o seu repositório local. Valeu Andery!                 \033[1;32m*\033[0m\n";
     cout << "\032[1;32m*                                                 *\033[0m\n";
     cout << "\032[1;32m***************************************************\033[0m\n";
 
